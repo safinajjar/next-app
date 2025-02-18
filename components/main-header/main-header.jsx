183753fb638419f4cbc4b90daa113854dd7f5356
Musti <safi.najjar1@gmail.com>
2025-02-18 19:59:10 +0100

    fix priority prop

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -10,7 +10,7 @@ export default function MainHeader() {
       <MainHeaderBackground />
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
-          <Image src={logoImg} alt="A plate with food on it" property />
+          <Image src={logoImg} alt="A plate with food on it" priority />
           NextLevel Food
         </Link>
 
